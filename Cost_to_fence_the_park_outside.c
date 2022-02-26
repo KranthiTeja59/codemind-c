c4dc56ebe3ee973f8605c2e1b99b77fc7277298c
KranthiTeja59 <96741610+KranthiTeja59@users.noreply.github.com>
2022-02-26 15:51:06 +0530

    #include<stdio.h> #include<math.h> int main() {     int L,B,W,C,l1,b1,area;     scanf("%d%d%d%d",&L,&B,&W,&C);     l1=L+2*W;     b1=B+2*W;     area=abs((L*B)-(l1*b1))*C;     if(l1<=0 ||b1<=0)     printf("Impossible");     else     printf("%d",area);     return 0; }

---
+++ Cost_to_fence_the_park_outside.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int L,B,W,C,l1,b1,area;
+    scanf("%d%d%d%d",&L,&B,&W,&C);
+    l1=L+2*W;
+    b1=B+2*W;
+    area=abs((L*B)-(l1*b1))*C;
+    if(l1<=0 ||b1<=0)
+    printf("Impossible");
+    else
+    printf("%d",area);
+    return 0;
+}
